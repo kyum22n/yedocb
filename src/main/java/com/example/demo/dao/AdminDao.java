63c7940444fdf3306207e7f42c0082b59e7cfd63
kyum22n <aphrod207@naver.com>
2026-05-05 19:50:54 +0900

    feat: 관리자 계정 dao&xml 생성

--- src/main/java/com/example/demo/dao/AdminDao.java
+++ src/main/java/com/example/demo/dao/AdminDao.java
@@ -1,5 +1,6 @@
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import com.example.demo.entity.Admin;
 
 /**
@@ -17,19 +18,25 @@ public interface AdminDao {
 
     // 모든 관리자 목록 조회
 	public List<Admin> selectAllAdmins();
-	// 관리자 1명 조회
-	public Admin selectAdminByAId(int aId);
+
+	// 관리자 상세 조회
+	public Admin selectAdminById(Integer adminId);
+	
 	// 관리자 등록
 	public int insertAdmin(Admin admin);
-    // 관리자 정보 수정
+    
+	// 관리자 정보 수정
     public int updateAdmin(Admin admin);
+	
 	// 관리자 삭제
-	public int deleteAdmin(int aId);
+	public int deleteAdmin(Integer adminId);
 
     // 로그인 ID로 관리자 조회
-    public Admin selectAdminByLoginId(String aLoginId);
+    public Admin selectAdminByLoginId(String adminLoginId);
+
 	// 관리자 이메일로 관리자 아이디 찾기
-	public String selectAdminIdByEmail(String aEmail);
+	public String selectAdminIdByEmail(String adminEmail);
+	
 	// 관리자 비밀번호 변경
-	public int updatePassword(Admin admin);
+	public int updatePassword(@Param("adminId") Integer adminId, @Param("adminPassword") String adminPassword);
 }
