873928dbb13255dbbfd83400cd9c35be3843b9fd
kyum22n <aphrod207@naver.com>
2026-05-07 15:16:59 +0900

    feat: 고객 관리 dao 생성

---
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -0,0 +1,19 @@
+package com.example.demo.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.example.demo.entity.Member;
+
+/**
+ * 파일명: MemberDao.java
+ * 설명: 사용자 정보 관련 DAO 인터페이스
+ *
+ * ===============================
+ * 수정 이력
+ * ===============================
+ * 2026-05-02 | 규민 | 인터페이스 생성
+ */
+
+@Mapper
+public interface MemberDao {
+    
+}
